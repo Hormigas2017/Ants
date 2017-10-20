195490da25a7ee05cfc81b2ee0c9170a65d536d3
danialarcon0698 <daniel.alarcon@upb.edu.co>
2017-10-19 22:57:55 -0500

    canvas

---
+++ Ants/Assets/Scripts/Generators/CreateButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateButton : MonoBehaviour
+{
+    InputField mInputField;
+    string stringNumber;
+    char[] charArray;
+
+    // Use this for initialization
+    void Start ()
+    {
+        mInputField = GetComponent<InputField>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        NumberOFAnts();
+
+    }
+
+    public void NumberOFAnts()
+    {
+        if (mInputField.text != null)
+        {
+            mInputField.text = stringNumber;
+        }
+
+        for (int i = 0; i <= charArray.Length; i++)
+        {
+            charArray = stringNumber.ToCharArray();
+        }
+        Debug.Log(charArray);
+    }
+}
--- Ants/Assets/Scripts/Generators/Generators.cs
+++ Ants/Assets/Scripts/Generators/Generators.cs
@@ -52,10 +52,12 @@ public class Generators : MonoBehaviour
                 boolAg = true;
                 ArchersGen();
             }
-            else
+            /*
+              else
             {
                 boolAg = false;
             }
+            */
         }
     }
 
