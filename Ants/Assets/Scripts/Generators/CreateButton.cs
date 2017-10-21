0344047954d8f74f42087d1f98578baa100840ec
danialarcon0698 <daniel.alarcon@upb.edu.co>
2017-10-20 22:02:53 -0500

    intento de canvas

--- Ants/Assets/Scripts/Generators/CreateButton.cs
+++ Ants/Assets/Scripts/Generators/CreateButton.cs
@@ -5,33 +5,40 @@ using UnityEngine.UI;
 
 public class CreateButton : MonoBehaviour
 {
-    InputField mInputField;
-    string stringNumber;
-    char[] charArray;
+    Transform mTrans;
+    Transform mCanvas;
+
+    Image antAG;
+
+    float t = 0.0f;
+    public Image[] imagesArray = new Image[10];
 
     // Use this for initialization
     void Start ()
     {
-        mInputField = GetComponent<InputField>();
+        antAG = GameObject.Find("AntAG").GetComponent<Image>();
+        mTrans = GameObject.Find("AntAG").GetComponent<Transform>();
+        mCanvas = GameObject.Find("WhiteBackGround").GetComponent<Transform>();
     }
 	
 	// Update is called once per frame
-	void Update () {
-        NumberOFAnts();
-
+	void Update ()
+    {
+        t += Time.deltaTime;
     }
 
-    public void NumberOFAnts()
+    public void NumAntsAG()
     {
-        if (mInputField.text != null)
+        for (int i = 0; i < imagesArray.Length; i++)
         {
-            mInputField.text = stringNumber;
+            if (t > 1)
+            {
+                float xy = 5f;
+                Vector3 changePos = new Vector3(xy, xy, 0);
+                imagesArray[i] = Instantiate(antAG, mCanvas.position, Quaternion.identity);
+                t = 0f;
+            }
+            i++;
         }
-
-        for (int i = 0; i <= charArray.Length; i++)
-        {
-            charArray = stringNumber.ToCharArray();
-        }
-        Debug.Log(charArray);
     }
 }
