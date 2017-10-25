edd82c93d36520d7e6cb81d2bc02358cf07ee457
JuanD2011 <juan.atehortual@upb.edu.co>
2017-10-25 00:29:03 -0500

    Camera01

---
+++ Ants/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector3 position = transform.position;
+
+
+        if(Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            position.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= panBorderThickness)
+        {
+            position.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            position.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x <= panBorderThickness)
+        {
+            position.x -= panSpeed * Time.deltaTime;
+        }
+
+        //position.x = Mathf.Clamp();
+
+        transform.position = position;
+
+
+    }
+}
